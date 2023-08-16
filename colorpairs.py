5ec9da53bbcc3156446fd9ae1310ae6e1690ee67
unknown <tim.fabian.berti@gmail.com>
2023-08-16 12:20:36 +0200

    color pair scan

---
+++ colorpairs.py
@@ -0,0 +1,82 @@
+import colour
+import csv
+import numpy as np
+from itertools import product
+from tqdm import tqdm
+
+
+def color_distance(color1, color2):
+    return np.linalg.norm(color1 - color2)
+
+
+def color_pair_matches(color1, color2, threshold):
+    return color_distance(color1, color2) <= threshold
+
+
+def color_pair_matches_only_with_color_vision_deficiency(color1, color2, M, threshold):
+    transformed_color1 = M @ color1
+    transformed_color2 = M @ color2
+    return color_pair_matches(transformed_color1, transformed_color2, threshold) and not color_pair_matches(color1, color2, threshold)
+
+
+def save_matching_pairs(matching_pairs, color_vision_deficency):
+    with open(f'{color_vision_deficency}.txt', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(matching_pairs)
+
+
+def scan_grid_for_matching_pairs(grid, color_vision_deficency, threshold=1):
+    M = colour.blindness.matrix_cvd_Machado2009(color_vision_deficency, 1)
+
+    matching_pairs = []
+
+    for color_pair in tqdm(grid, total=128**4):
+        if color_pair_matches_only_with_color_vision_deficiency(*color_pair, M, threshold):
+            matching_pairs.append(
+                [*(tuple(color) for color in color_pair), color_distance(*color_pair)])
+
+    save_matching_pairs(matching_pairs, color_vision_deficency)
+
+
+def main():
+
+    reds_no_green = np.array(
+        list(product(range(128, 256), [0], range(0, 128))))
+    reds_no_blue = np.array(
+        list(product(range(128, 256), range(0, 128), [0])))
+    greens_no_blue = np.array(
+        list(product(range(0, 128), range(128, 256), [0])))
+    greens_no_red = np.array(
+        list(product([0], range(128, 256), range(0, 128))))
+    blues_no_red = np.array(
+        list(product([0], range(0, 128), range(128, 256))))
+    blues_no_green = np.array(
+        list(product(range(0, 128), [0], range(128, 256))))
+
+    color_vision_deficency = 'Protanomaly'
+    grid = product(greens_no_blue, greens_no_red)
+    scan_grid_for_matching_pairs(grid, color_vision_deficency)
+
+    color_vision_deficency = 'Protanomaly'
+    grid = product(blues_no_red, blues_no_green)
+    scan_grid_for_matching_pairs(grid, color_vision_deficency)
+
+    color_vision_deficency = 'Deuteranomaly'
+    grid = product(reds_no_blue, reds_no_green)
+    scan_grid_for_matching_pairs(grid, color_vision_deficency)
+
+    color_vision_deficency = 'Deuteranomaly'
+    grid = product(blues_no_red, blues_no_green)
+    scan_grid_for_matching_pairs(grid, color_vision_deficency)
+
+    color_vision_deficency = 'Tritanomaly'
+    grid = product(reds_no_blue, reds_no_green)
+    scan_grid_for_matching_pairs(grid, color_vision_deficency)
+
+    color_vision_deficency = 'Tritanomaly'
+    grid = product(greens_no_blue, greens_no_red)
+    scan_grid_for_matching_pairs(grid, color_vision_deficency)
+
+
+if __name__ == '__main__':
+    main()
