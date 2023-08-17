425672a31a487c94762c81ab2219f96991dd4bac
unknown <tim.fabian.berti@gmail.com>
2023-08-17 09:15:32 +0200

    refactoring

--- colorpairs.py
+++ colorpairs.py
@@ -5,6 +5,19 @@ from itertools import product
 from tqdm import tqdm
 
 
+reds_no_green = np.array(
+    list(product(range(128, 256), [0], range(0, 128))))
+reds_no_blue = np.array(
+    list(product(range(128, 256), range(0, 128), [0])))
+greens_no_blue = np.array(
+    list(product(range(0, 128), range(128, 256), [0])))
+greens_no_red = np.array(
+    list(product([0], range(128, 256), range(0, 128))))
+blues_no_red = np.array(
+    list(product([0], range(0, 128), range(128, 256))))
+blues_no_green = np.array(
+    list(product(range(0, 128), [0], range(128, 256))))
+
 def save_matching_pairs(matching_pairs, file_name):
     with open(f'{file_name}.csv', 'w', newline='') as f:
         writer = csv.writer(f)
@@ -13,7 +26,7 @@ def save_matching_pairs(matching_pairs, file_name):
         writer.writerows(matching_pairs)
 
 
-def scan_grid_for_matching_pairs(grid, color_vision_deficency, file_name, threshold=0):
+def scan_grid_for_matching_pairs(grid, color_vision_deficency, file_name, threshold):
     M = colour.blindness.matrix_cvd_Machado2009(color_vision_deficency, 1)
 
     matching_pairs = []
@@ -21,8 +34,8 @@ def scan_grid_for_matching_pairs(grid, color_vision_deficency, file_name, thresh
     print(f'{file_name}')
     for color_pair in tqdm(grid, total=128**4):
         color1, color2 = color_pair
-        transformed_color1 = np.rint(M @ color1).astype(int)
-        transformed_color2 = np.rint(M @ color2).astype(int)
+        transformed_color1 = np.rint(M @ color1).astype(int).clip(0, 255)
+        transformed_color2 = np.rint(M @ color2).astype(int).clip(0, 255)
 
         distance = np.linalg.norm(color1 - color2)
         transformed_distance = np.linalg.norm(
@@ -33,51 +46,24 @@ def scan_grid_for_matching_pairs(grid, color_vision_deficency, file_name, thresh
 
     save_matching_pairs(matching_pairs, file_name)
 
+def scan_colorspace(color_vision_deficency, threshold=0):
+    grid = product(reds_no_blue, reds_no_green, threshold)
+    scan_grid_for_matching_pairs(
+        grid, color_vision_deficency, f'{color_vision_deficency}_red')
+
+    grid = product(greens_no_blue, greens_no_red, threshold)
+    scan_grid_for_matching_pairs(
+        grid, color_vision_deficency, f'{color_vision_deficency}_green')
+
+    grid = product(blues_no_green, blues_no_red, threshold)
+    scan_grid_for_matching_pairs(
+        grid, color_vision_deficency, f'{color_vision_deficency}_blue')
+
 
 def main():
-
-    reds_no_green = np.array(
-        list(product(range(128, 256), [0], range(0, 128))))
-    reds_no_blue = np.array(
-        list(product(range(128, 256), range(0, 128), [0])))
-    greens_no_blue = np.array(
-        list(product(range(0, 128), range(128, 256), [0])))
-    greens_no_red = np.array(
-        list(product([0], range(128, 256), range(0, 128))))
-    blues_no_red = np.array(
-        list(product([0], range(0, 128), range(128, 256))))
-    blues_no_green = np.array(
-        list(product(range(0, 128), [0], range(128, 256))))
-
-    color_vision_deficency = 'Protanomaly'
-    grid = product(greens_no_blue, greens_no_red)
-    scan_grid_for_matching_pairs(
-        grid, color_vision_deficency, 'Protanomaly_green')
-
-    color_vision_deficency = 'Protanomaly'
-    grid = product(blues_no_red, blues_no_green)
-    scan_grid_for_matching_pairs(
-        grid, color_vision_deficency, 'Protanomaly_blue')
-
-    color_vision_deficency = 'Deuteranomaly'
-    grid = product(reds_no_blue, reds_no_green)
-    scan_grid_for_matching_pairs(
-        grid, color_vision_deficency, 'Deuteranomaly_red')
-
-    color_vision_deficency = 'Deuteranomaly'
-    grid = product(blues_no_red, blues_no_green)
-    scan_grid_for_matching_pairs(
-        grid, color_vision_deficency, 'Deuteranomaly_blue')
-
-    color_vision_deficency = 'Tritanomaly'
-    grid = product(reds_no_blue, reds_no_green)
-    scan_grid_for_matching_pairs(
-        grid, color_vision_deficency, 'Tritanomaly_red')
-
-    color_vision_deficency = 'Tritanomaly'
-    grid = product(greens_no_blue, greens_no_red)
-    scan_grid_for_matching_pairs(
-        grid, color_vision_deficency, 'Tritanomaly_green')
+    scan_colorspace('Protanopia')
+    scan_colorspace('Deuteranopia')
+    scan_colorspace('Tritanopia', threshold=2)
 
 
 if __name__ == '__main__':
